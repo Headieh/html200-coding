c08304dcd11349b4f71da068ea9bd248d9d3ff27
Nichole Godwin <headiehg@mail.com>
2020-03-05 21:47:29 -0800

    lesson2 assignment1

---
+++ lesson02/calculator.js
@@ -0,0 +1,13 @@
+//Lesson 2 Exercise 1
+
+let h = 36;
+const i = '12';
+let j = 2;
+let k = 6;
+let l = 3;
+
+console.log((h/k) + i);
+console.log(h - l);
+console.log(j * (h / k));
+console.log(l ** j);
+console.log(l % j);
