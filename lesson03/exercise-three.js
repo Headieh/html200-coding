b9276b15fe47a445cc7c6b6618af82448a6d3582
Nichole Godwin <headiehg@mail.com>
2020-03-09 13:41:08 -0700

    lesson3 assignment3

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,7 @@
+//Use a for loop to console log all of the elements plus the index and a comma
+
+let food = ['grains', 'vegetables', 'fruit', 'protein'];
+
+for (let i = 0; i < food.length; i++) {
+	console.log(`${i},${food[i]}`);
+}
