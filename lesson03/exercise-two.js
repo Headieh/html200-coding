3b8d8232081b6efca1aefc0ce6ede5e50ee7d92a
Nichole Godwin <headiehg@mail.com>
2020-03-09 13:24:49 -0700

    lesson3 assignment2

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,5 @@
+//Take a sentence of at least five words and a period.
+const statement = 'Fruits are my favorite foods!';
+//Make an array from that sentence and console.log the third word.
+const statementArr = statement.split(' ');
+console.log(statementArr[2]);
