7da793d313d673855ca2ab71a5d192e31ecdcc4f
Nichole Godwin <headiehg@mail.com>
2020-03-10 22:02:38 -0700

    lesson4 exercise1

---
+++ lesson04/exercise-one.js
@@ -0,0 +1,14 @@
+function square() {
+	let input = prompt('Enter a number');
+	let result = 0;
+	result = Number(input) ** 2;
+	alert(result);
+}
+
+function multiply() {
+	let input1 = prompt('Enter 1st number');
+	let input2 = prompt('Enter 2nd number');
+	let result = 0;
+	result = Number(input1) * Number(input2);
+	alert(result);
+}
